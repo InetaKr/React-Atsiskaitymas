c6c8673279b72f455fa93c7c87c45f199749ed01
InetaKr <ineta.kriunaite@outlook.com>
2023-02-03 07:26:01 +0200

    footer: miner changes, styles added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,13 @@
-import { Link } from "react-router-dom";
-import logo from '../media/logo.png'
-
 const Footer = () => {
   
   return (
     <>
-    <footer className="footer-wrapper">
+    <footer>
+      <div className="footer-wrapper">
       <div className="footer-first-section">
-        <div className="footerLogo">
-          <Link to= "/">
-            <img src={logo} alt="logo" />
-            </Link>
-        </div>
         <div className="footer-text">
-          <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
-          elit,Lorem ipsum dolor sit amet.
+          <p>You Love Cats...WE Love Cats..Everyone here Loves Cats. <br /> If you Love Dogs you do not belong here. <br />
+            Let's share our Love for Cats Together !
           </p>
         </div>
         <div className="footer-social-icon">
@@ -24,7 +17,7 @@ const Footer = () => {
           <a href="#"><i className="fa fa-google-plus google-bg"></i></a>
         </div>
       </div>
-      <div className="footer-second-section">
+      <div className="footer-second-section">     
         <div className="companyInfo">
           <i className="fa fa-map-marker"></i>
           <div className="companyInfo-text">
@@ -37,27 +30,32 @@ const Footer = () => {
           <div className="companyInfo-text">
             <h4>Call us</h4>
             <span>+37068123123</span>
-          </div>
+        </div>
+        </div>
         <div className="companyInfo">
           <i className="fa fa-envelope-open"></i>
           <div className="companyInfo-text">
             <h4>Mail Us</h4>
             <span>CatLovers@info.com</span>
           </div>
-        </div>  
+        </div>
       </div>
       <div className="third-section">
-        <div className="third-section-headeing">
+      <div className="footer-heading">
+        <h3>Subscribe</h3>
+      </div>
+        <div className="footer-text">
           <p>Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
         </div>
-        <div className="subscribe-form">
-          <form action="#">
+          <form action="#" className="subscribe-form">
             <input type="email" placeholder="Email Address" />
             <button><i className="fa fa-send" /></button>
           </form>
-        </div>
+      </div> 
       </div>
-    </div>     
+      <div className="copyright-text">
+        <p>Copyright &copy; 2018, All Right Reserved CatLovers</p>
+      </div>   
   </footer>
     </>
   );
