10951abcea7c3a9576d3e62f065caab67de59462
InetaKr <ineta.kriunaite@outlook.com>
2023-02-03 02:03:15 +0200

    added loading and no post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,22 +1,30 @@
 import Post from "./Post";
 import PostContext from "../context/PostContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const Posts = () => {
     const { posts } = useContext(PostContext);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        setLoading(false);
+    }, [posts]);
+    
     return(
         <>
-          {
-            posts.map((post, index) =>
+          {loading?(
+            <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="loading"/>
+          ) : posts.length === 0 ? (
+              <p>No Posts Created</p>
+            ) : (
+              posts.map((post, index) =>(
                 <Post
                   key={post.id || index}
                   data={post} 
                 />
-            )
-          }
+              ))
+          )}
         </>
     );
 }
-
 export default Posts;
